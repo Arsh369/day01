54ca1e4f78d1b1a9cb814d7cbb12a83feff83482
bharatXjaidiya <jaidiyabharat16@gmail.com>
2026-01-09 16:34:03 +0530

    updated info page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-
+import Info from './pages/Info'
 const App = () => {
   return (
-    <div>
-      <h1 className='text-3xl font-bold'>Tasks</h1>
-      <h1 className='py-5'><span className='text-2xl font-bold'>Abhishek</span>-Make anything at about page . Create branch "abhi-test" branch and do your task</h1>
-      <h1><span className='text-2xl font-bold'>Bharat</span>-Make anything at info page . Create branch "bharat-test" branch and do your task</h1>
+    <div className='app'>
+      <Info></Info>
     </div>
   )
 }
--- src/pages/Info.jsx
+++ src/pages/Info.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
-
-const Info = () => {
+import img1 from '../assets/1.jpg'
+import img2 from '../assets/2.jpg'
+import './Info.css'
+const About = () => {
   return (
-    <div>Info</div>
+    <div className="about">
+      <div className="card">
+        <img src={img1} alt="" />
+        <h1>Let's create the amazing "STUFF"</h1>
+      </div>
+      <div className="card">
+        <img src={img2} alt="" />
+        <h1>Oh Really ? Ok Let's do it and "FUCK IT"</h1>
+      </div>
+    </div>
   )
 }
 
-export default Info
+export default About
