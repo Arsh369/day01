386c57bb6e8566f250baa5d2719f1e3e85713a9b
Arsh369 <playgames1797@gmail.com>
2026-01-09 14:48:58 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-3xl font-bold'>Tasks</h1>
+      <h1 className='py-5'><span className='text-2xl font-bold'>Abhishek</span>-Make anything at '/about' route . Create branch "abhi-test" branch and do your task</h1>
+      <h1><span className='text-2xl font-bold'>Bharat</span>-Make anything at '/info' route . Create branch "bharat-test" branch and do your task</h1>
+    </div>
+  )
+}
+
+export default App
