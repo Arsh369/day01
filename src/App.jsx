c5e06f0055711b4df8072579dde02925de823872
Arsh369 <playgames1797@gmail.com>
2026-01-09 14:59:22 +0530

    some changes in task

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ const App = () => {
   return (
     <div>
       <h1 className='text-3xl font-bold'>Tasks</h1>
-      <h1 className='py-5'><span className='text-2xl font-bold'>Abhishek</span>-Make anything at '/about' route . Create branch "abhi-test" branch and do your task</h1>
-      <h1><span className='text-2xl font-bold'>Bharat</span>-Make anything at '/info' route . Create branch "bharat-test" branch and do your task</h1>
+      <h1 className='py-5'><span className='text-2xl font-bold'>Abhishek</span>-Make anything at about page . Create branch "abhi-test" branch and do your task</h1>
+      <h1><span className='text-2xl font-bold'>Bharat</span>-Make anything at info page . Create branch "bharat-test" branch and do your task</h1>
     </div>
   )
 }
